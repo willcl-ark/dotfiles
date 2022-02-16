6784a28ea2479ed598f04be554dca4f38a66202a
willcl-ark <will8clark@gmail.com>
2022-02-16 09:02:43 +0000

    Add asciidoctor.nvim

---
+++ nvim/.config/nvim/lua/user/asciidoctor.lua
@@ -0,0 +1,11 @@
+vim.cmd [[
+  " List of filetypes to highlight, default `[]`
+  let g:asciidoctor_fenced_languages = ['python', 'c', 'cpp', 'rust']
+
+  " Fold sections, default `0`.
+  let g:asciidoctor_folding = 0
+
+  " Fold options, default `0`.
+  let g:asciidoctor_fold_options = 1
+]]
+
