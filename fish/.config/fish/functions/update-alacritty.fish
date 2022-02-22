44a524ab3e2ccca937c1a76a990473c5b6ecec98
willcl-ark <will8clark@gmail.com>
2022-02-22 10:05:45 +0000

    use rename to update in-use binary
    Update-alacritty was not installing properly as the binary was in use by
    the terminal itself.
    
    Using rename() means that the binary can be atomically updated which
    will be applied to the running process next time it is restarted.

--- fish/.config/fish/functions/update-alacritty.fish
+++ fish/.config/fish/functions/update-alacritty.fish
@@ -1,14 +1,23 @@
 function update-alacritty
-   # Build
+   # Build & install
     cargo build --release && \
-    sudo cp target/release/alacritty /usr/local/bin && \
-    ## Terminfo
+    # copy to a temp location
+    sudo cp target/release/alacritty /usr/local/bin/alacritty-new && \
+    # use `rename` to atomically rename the file: https://man7.org/linux/man-pages/man2/rename.2.html
+    sudo rename /usr/local/bin/alacritty-new /usr/local/bin/alacritty && \
+    # Terminfo
     sudo tic -xe alacritty,alacritty-direct extra/alacritty.info && \
-    ## Desktop Entry (Ubuntu)
-    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg && \
-    sudo desktop-file-install extra/linux/Alacritty.desktop && \
-    sudo update-desktop-database && \
-    ## Completions
+    # Desktop Entry (Ubuntu)
+    if test (uname -s) = Linux
+        sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg && \
+        sudo desktop-file-install extra/linux/Alacritty.desktop && \
+        sudo update-desktop-database
+    end
+    # Manpages
+    sudo mkdir -p /usr/local/share/man/man1 && \
+    gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null && \
+    gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null && \
+    # Completions
     mkdir -p $fish_complete_path[1] && \
     cp extra/completions/alacritty.fish $fish_complete_path[1]/alacritty.fish
 end
