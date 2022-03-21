c40214ca90f625c163a3a2b98ca7401b42032481
willcl-ark <will8clark@gmail.com>
2022-03-21 13:04:25 +0000

    Remove whichkey plugin
    Leaving uncommented caused :checkhealth to always try and check it

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -51,7 +51,7 @@ return packer.startup(function(use)
   use "lewis6991/impatient.nvim"
   use "lukas-reineke/indent-blankline.nvim"
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
-  use "folke/which-key.nvim"
+  -- use "folke/which-key.nvim"
   use "habamax/vim-asciidoctor"
 
   -- Colorschemes
