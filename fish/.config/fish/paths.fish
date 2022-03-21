8cd733c0238eb4ead81b07c4957b5db0f864b1a2
willcl-ark <will8clark@gmail.com>
2022-03-21 13:22:19 +0000

    Add $HOME/.gems directory to path

--- fish/.config/fish/paths.fish
+++ fish/.config/fish/paths.fish
@@ -1,9 +1,15 @@
+# paths added later will be PREPENDED to current path
+# Add paths to the bottom to have them appear first
+
+# This fixed my flatpak firefox install on Ubuntu
 if [ (uname) = Linux ]
     set -gx DXG_DATA_DIRS /usr/share/ubuntu:/home/will/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:/var/lib/snapd/desktop
 end
+
 set -g fish_user_paths
-fish_add_path $HOME/.local/share/gem/ruby/2.7.0/bin 
+
 fish_add_path $HOME/.npm-packages/bin 
+fish_add_path $HOME/.gems/bin
 if [ (uname) = Linux ]
     fish_add_path /home/linuxbrew/.linuxbrew/bin 
 end
@@ -13,8 +19,9 @@ fish_add_path $HOME/go/bin
 fish_add_path /usr/local/go/bin 
 fish_add_path $HOME/.pyenv/bin 
 fish_add_path $HOME/.cargo/bin 
-fish_add_path $HOME/neovim/bin 
 fish_add_path /usr/lib/ccache
+# homebrew
 if [ (uname) = Darwin ]
     fish_add_path /usr/local/bin
 end
+fish_add_path $HOME/neovim/bin 
