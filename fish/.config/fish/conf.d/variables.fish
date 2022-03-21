8a3dd1651ffa0d4aee52f71a280890f93a2d9e82
willcl-ark <will8clark@gmail.com>
2022-03-21 13:15:56 +0000

    Allow pip installation outside of virtualenvs
    Was annoying to pip-install system python tools
    Still uses the pyenv system-default environment, and not the _actual_
    system default python

--- fish/.config/fish/conf.d/variables.fish
+++ fish/.config/fish/conf.d/variables.fish
@@ -13,7 +13,7 @@ set -gx GUIX_SIGS_REPO $HOME/src/guix.sigs
 set -gx MANPATH $HOME/.nvm/versions/node/v16.13.1/share/man $HOME/.npm-packages/share/man /usr/man /usr/share/man /usr/local/man /usr/local/share/man /usr/X11R6/man /opt/man /snap/man
 set -gx NPM_PACKAGES $HOME/.npm-packages
 set -gx NVM_DIR $HOME/.nvm
-set -gx PIP_REQUIRE_VIRTUALENV 1
+set -gx PIP_REQUIRE_VIRTUALENV 0
 set -gx SIGNER will8clark
 set -gx VIRTUALFISH_DEFAULT_PYTHON (command -v python3)
 set -gx VISUAL nvim
