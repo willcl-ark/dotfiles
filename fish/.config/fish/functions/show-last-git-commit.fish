397f1ec75f16e22d8f4d6018f6ee3a64246b187e
willcl-ark <will8clark@gmail.com>
2022-04-26 21:50:06 +0100

    show-last-git-commit.fish

---
+++ fish/.config/fish/functions/show-last-git-commit.fish
@@ -0,0 +1,16 @@
+function show-last-git-commit
+    for dir in (fd --type d --maxdepth 1)
+        pushd $dir
+        echo "---------------------------"
+        # checkout the most-recently-updated branch
+        git fetch --all > /dev/null
+        set --local cur_branch (git branch | head -n 1 | sed 's/[^ ]* //')
+        set --local best_branch (git branch --sort=-committerdate | head -n 1 | sed 's/[^ ]* //')
+        if [ $cur_branch != $best_branch ]
+            git checkout $best_branch > /dev/null
+        end
+        git log head -1 | rg --regexp '^.*(Author|Date).*$' | tee
+        popd
+    end
+    echo "---------------------------"
+end
