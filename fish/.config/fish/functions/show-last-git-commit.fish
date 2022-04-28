79c70b112faa0eab4af79feb5bd5554ade140716
willcl-ark <will8clark@gmail.com>
2022-04-28 12:43:00 +0100

    fix head of show-last-commit

--- fish/.config/fish/functions/show-last-git-commit.fish
+++ fish/.config/fish/functions/show-last-git-commit.fish
@@ -3,13 +3,14 @@ function show-last-git-commit
         pushd $dir
         echo "---------------------------"
         # checkout the most-recently-updated branch
-        git fetch --all > /dev/null
+        git fetch --all --tags --prune > /dev/null
         set --local cur_branch (git branch | head -n 1 | sed 's/[^ ]* //')
         set --local best_branch (git branch --sort=-committerdate | head -n 1 | sed 's/[^ ]* //')
         if [ $cur_branch != $best_branch ]
             git checkout $best_branch > /dev/null
         end
-        git log head -1 | rg --regexp '^.*(Author|Date).*$' | tee
+        git pull > /dev/null
+        git log HEAD -1 | rg --regexp '^.*(Author|Date).*$' | tee
         popd
     end
     echo "---------------------------"
