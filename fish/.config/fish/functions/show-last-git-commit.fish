36ac957b4361497b9963d57907f052af588c8964
willcl-ark <will8clark@gmail.com>
2022-05-06 01:06:42 +0100

    fix lat-git-commit func

--- fish/.config/fish/functions/show-last-git-commit.fish
+++ fish/.config/fish/functions/show-last-git-commit.fish
@@ -2,12 +2,12 @@ function show-last-git-commit
     for dir in (fd --type d --maxdepth 1)
         pushd $dir
         echo "---------------------------"
-        # checkout the most-recently-updated branch
-        git fetch --all --tags --prune > /dev/null
-        set --local cur_branch (git branch | head -n 1 | sed 's/[^ ]* //')
-        set --local best_branch (git branch --sort=-committerdate | head -n 1 | sed 's/[^ ]* //')
-        if [ $cur_branch != $best_branch ]
-            git checkout $best_branch > /dev/null
+        git fetch --all > /dev/null
+        # Coul use `git branch --show-current` here too?
+        set --local branch_cur (git rev-parse --abbrev-ref HEAD)
+        set --local branch_best (git for-each-ref --count=1 --sort=-committerdate refs/heads/ --format='%(refname:short)')
+        if [ $branch_cur != $branch_best ]
+            git switch $best_branch > /dev/null
         end
         git pull > /dev/null
         git log HEAD -1 | rg --regexp '^.*(Author|Date).*$' | tee
