85ec95c765c745e94c67e326205d1046b63d0995
willcl-ark <will8clark@gmail.com>
2022-05-10 14:27:57 +0100

    Fix show-last-git-commit
    Now handles all remote branches properly and switches to them while
    maintaining tracking

--- fish/.config/fish/functions/show-last-git-commit.fish
+++ fish/.config/fish/functions/show-last-git-commit.fish
@@ -2,12 +2,12 @@ function show-last-git-commit
     for dir in (fd --type d --maxdepth 1)
         pushd $dir
         echo "---------------------------"
+        echo (basename $PWD)
         git fetch --all > /dev/null
-        # Coul use `git branch --show-current` here too?
-        set --local branch_cur (git rev-parse --abbrev-ref HEAD)
-        set --local branch_best (git for-each-ref --count=1 --sort=-committerdate refs/heads/ --format='%(refname:short)')
+        set --local branch_cur (git branch --show-current)
+        set --local branch_best (git for-each-ref --count=1 --sort=-committerdate --format='%(refname:short)')
         if [ $branch_cur != $branch_best ]
-            git switch $best_branch > /dev/null
+            git switch --track $branch_best > /dev/null
         end
         git pull > /dev/null
         git log HEAD -1 | rg --regexp '^.*(Author|Date).*$' | tee
