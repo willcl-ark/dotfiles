ae8eff3aafea15e73d6c4ba3b12a9721df607ef9
willcl-ark <will8clark@gmail.com>
2022-06-21 14:58:06 +0100

    nvim: lualine use default colorscheme

--- nvim/.config/nvim/lua/user/lualine.lua
+++ nvim/.config/nvim/lua/user/lualine.lua
@@ -82,6 +82,7 @@ lualine.setup({
     options = {
         icons_enabled = true,
         theme = "auto",
+        --theme = "solarized",
         component_separators = { left = "", right = "" },
         section_separators = { left = "", right = "" },
         disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
