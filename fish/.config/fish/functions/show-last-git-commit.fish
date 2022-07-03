41994371f92c6054dcd490e187c1d6f4f5245e5d
willcl-ark <will8clark@gmail.com>
2022-07-03 20:23:55 +0100

    fish: show last git commit

--- fish/.config/fish/functions/show-last-git-commit.fish
+++ fish/.config/fish/functions/show-last-git-commit.fish
@@ -3,14 +3,19 @@ function show-last-git-commit
         pushd $dir
         echo "---------------------------"
         echo (basename $PWD)
-        git fetch --all > /dev/null
-        set --local branch_cur (git branch --show-current)
-        set --local branch_best (git for-each-ref --count=1 --sort=-committerdate --format='%(refname:short)')
-        if [ $branch_cur != $branch_best ]
-            git switch --track $branch_best > /dev/null
-        end
-        git pull > /dev/null
-        git log HEAD -1 | rg --regexp '^.*(Author|Date).*$' | tee
+        git fetch origin > /dev/null
+        #set --local branch_cur (git branch --show-current)
+        #set --local branch_best (git for-each-ref --count=1 --sort=-committerdate --format='%(refname:short)')
+        #if [ $branch_cur != $branch_best ]
+        #    git switch --track $branch_best > /dev/null
+        #end
+        #git pull > /dev/null
+        #git log HEAD -1 | rg --regexp '^.*(Author|Date).*$' | tee
+        
+        # git show --pretty=short (git rev-list --all | head -1)
+
+        git fetch && git for-each-ref --sort=-committerdate --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))' --color=always | head -n 1
+        git shortlog -s -n --all --no-merges
         popd
     end
     echo "---------------------------"
