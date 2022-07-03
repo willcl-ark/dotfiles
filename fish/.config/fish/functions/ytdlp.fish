a6d2070cbe7fcf78ec411c5a70e78b38bdaf02c6
willcl-ark <will8clark@gmail.com>
2022-07-03 20:24:14 +0100

    fish: add menu to ytdlp

--- fish/.config/fish/functions/ytdlp.fish
+++ fish/.config/fish/functions/ytdlp.fish
@@ -2,5 +2,10 @@ function ytdlp
     read --prompt-str "Enter video URL: " url
     yt-dlp -F $url
     read --prompt-str "Enter format: " format
-    yt-dlp -f $format $url
+    read --prompt-str "Embed Thumbnail? (y/n)" thumb
+    if test "$thumb" = "y" -o "Y"
+        yt-dlp -f $format $url --embed-thumbnail
+    else
+        yt-dlp -f $format $url
+    end
 end
