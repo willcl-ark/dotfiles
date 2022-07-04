3486f35625c7395fc4f833a50f2e266665ca895c
willcl-ark <will8clark@gmail.com>
2022-07-04 14:43:45 +0100

    fish: update neovim use deps

--- fish/.config/fish/functions/update-neovim.fish
+++ fish/.config/fish/functions/update-neovim.fish
@@ -1,7 +1,5 @@
 function update-neovim
-    # For some reason ninja generates log files owned by root which prevent us from re-running make in the future
-    # Take ownership of them (all)
-    sudo chown -R will:will $HOME/src/neovim/
+    make deps
     make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim" CMAKE_BUILD_TYPE=Release -j16
     sudo make install
 end
