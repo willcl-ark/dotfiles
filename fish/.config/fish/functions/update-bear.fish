da718b57eea4b5541798dfd4aa5ee3561b54e12c
willcl-ark <will8clark@gmail.com>
2022-07-04 14:43:18 +0100

    fish: update bear out of source build

--- fish/.config/fish/functions/update-bear.fish
+++ fish/.config/fish/functions/update-bear.fish
@@ -1,5 +1,5 @@
 function update-bear
-    cmake -DENABLE_UNIT_TESTS=OFF -DENABLE_FUNC_TESTS=OFF $SRC/Bear
+    cmake -DENABLE_UNIT_TESTS=OFF -DENABLE_FUNC_TESTS=OFF $HOME/src/Bear
     make all
     sudo make install
 end
