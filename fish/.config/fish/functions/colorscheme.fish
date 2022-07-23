0ca6add418926d804c41dd4f7dc90315099d9aa0
willcl-ark <will8clark@gmail.com>
2022-07-23 20:23:09 +0100

    Fix colorscheme to handle different fonts

--- fish/.config/fish/functions/colorscheme.fish
+++ fish/.config/fish/functions/colorscheme.fish
@@ -3,7 +3,15 @@ function colorscheme
     set --local nvim $HOME/dotfiles/nvim/.config/nvim
 
     # Set alacritty color
-    cat $alacritty/base.yml $alacritty/$argv[1].yml > $alacritty/alacritty.yml
+    switch (uname -s)
+        case Linux:
+            cat $alacritty/base.yml > $alacritty/alacritty.yml
+        case Darwin
+            cat ~/dotfiles/alacritty/.config/alacritty/base.yml | sed 's/style: Regular/style: Text/' > alacritty.yml
+        case *
+            echo "uname returned unknown value"
+    end
+    cat $alacritty/$argv[1].yml >> $alacritty/alacritty.yml
 
     # Set neovim color
     echo "require \"user.$argv[1]\"" > $nvim/lua/user/colorscheme.lua
