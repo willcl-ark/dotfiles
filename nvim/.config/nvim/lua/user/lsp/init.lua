141af52dba92326b096c38ef963805196d1446af
willcl-ark <will8clark@gmail.com>
2022-07-24 22:47:02 +0100

    nvim: switch to mason for auto lsp installer
    nvim-lsp-installer is now unmaintained:
    https://github.com/williamboman/nvim-lsp-installer/discussions/876

--- nvim/.config/nvim/lua/user/lsp/init.lua
+++ nvim/.config/nvim/lua/user/lsp/init.lua
@@ -1,8 +1,2 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "user.lsp.lsp-installer"
-require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+require("user.lsp.lsp-config")
+require("user.lsp.mason")
