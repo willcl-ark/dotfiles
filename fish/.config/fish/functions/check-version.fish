a3532a32179bbb6b984a634019866d79c49cab93
willcl-ark <will8clark@gmail.com>
2022-10-05 12:11:59 +0100

    fish: update progs to latest tag only

--- fish/.config/fish/functions/check-version.fish
+++ fish/.config/fish/functions/check-version.fish
@@ -1,17 +1,21 @@
 function check-version
     # Returns 0 if it finds git tip in version string
     # Works for most programs
-    set ver_git (git log --oneline | head -n 1 | awk '{print $1;}')
-    echo "$argv[1] git at $ver_git"
+
+    # Checkout the most recent non-nightly tag
+    set --local TAG (git describe --tags --exclude "nightly" --abbrev=0 (git rev-list --tags --max-count=1))
+    git checkout $TAG
+
+    echo "$argv[1] git at $TAG"
     set ver_installed ($argv[1] --version)
     echo "$argv[1] installed at $ver_installed"
-    string match "*$ver_git*" $ver_installed
+    string match "*$TAG*" $ver_installed
     switch $status
         case 0
-            echo "$argv[1]: installed version matches tip"
+            echo "$argv[1]: installed version matches latest tag"
             return 0
         case 1
-            echo "$argv[1]: installed version behind tip"
+            echo "$argv[1]: installed version behind latest tag"
             return 1
     end
 end
