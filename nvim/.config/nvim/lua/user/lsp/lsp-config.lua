9d61cdef52b0aa11653381663361c984a6fb6b5f
willcl-ark <will@256k1.dev>
2022-11-01 14:00:13 +0000

    nvim: add clangd lsp setup

--- nvim/.config/nvim/lua/user/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-config.lua
@@ -41,11 +41,14 @@ local lsp_flags = {
     debounce_text_changes = 150,
 }
 
+local nvim_lsp = require("lspconfig")
+
 -- Setup a default handler for all installed servers
 require("mason-lspconfig").setup_handlers({
     -- The first entry (without a key) will be the default handler
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
+
     function(server_name) -- default handler (optional)
         require("lspconfig")[server_name].setup {
             on_attach = on_attach,
@@ -57,6 +60,14 @@ require("mason-lspconfig").setup_handlers({
         require("rust-tools").setup {
             on_attach = on_attach,
             flags = lsp_flags,
+        }
+    end,
+    ["clangd"] = function()
+        require("lspconfig").clangd.setup {
+            on_attach = on_attach,
+            flags = lsp_flags,
+            -- root_dir = nvim_lsp.util.find_git_ancestor(),
+            root_dir = nvim_lsp.util.root_pattern('.git', 'Makefile.am'),
         }
     end,
     ["sumneko_lua"] = function()
