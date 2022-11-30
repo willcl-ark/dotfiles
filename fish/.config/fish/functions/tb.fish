2123c18a71eaf2cc1a6abd9090bfba709fb4572a
willcl-ark <will@256k1.dev>
2022-11-30 13:31:01 +0000

    fish: fix termbin function
    wrap alias

--- fish/.config/fish/functions/tb.fish
+++ fish/.config/fish/functions/tb.fish
@@ -1,4 +1,3 @@
-# Defined in - @ line 0
-function tb --description 'alias tb=nc termbin.com 9999'
-	nc termbin.com 9999 $argv;
+function tb --wraps='nc termbin.com 9999' --description 'alias tb nc termbin.com 9999'
+  nc termbin.com 9999 $argv; 
 end
