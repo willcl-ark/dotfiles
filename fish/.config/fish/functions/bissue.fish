ddb065a92641181cab6858aa430b4f9f5e9afe43
willcl-ark <will@256k1.dev>
2023-01-18 16:05:14 +0000

    fish: add bissue function
    Creates a new temp dir and conf file for testing bitcoin core prs

---
+++ fish/.config/fish/functions/bissue.fish
@@ -0,0 +1,20 @@
+function bissue
+    if not set -q argv[1]
+        echo "provide an issue number"
+        return 1
+    end
+
+    set --local datadir "/tmp/bitcoin_$argv[1]"
+    mkdir -p $datadir
+    touch $datadir/bitcoin.conf
+    echo "Do you want to edit bitcoin.conf? Yes [1] No [2]"
+    read --local --prompt-str "> " edit
+
+    switch $edit
+        case 1
+            nvim $datadir/bitcoin.conf
+        case '*'
+    end
+
+    commandline "/home/will/src/bitcoin/src/bitcoind -datadir=$datadir"
+end
