64f3ed38a42b557ac0a2f76c0a212767161a9d31
willcl-ark <will@256k1.dev>
2023-01-20 21:56:49 +0000

    fish: speedup bear & neovim compile

--- fish/.config/fish/functions/update-bear.fish
+++ fish/.config/fish/functions/update-bear.fish
@@ -1,5 +1,5 @@
 function update-bear
     cmake -DENABLE_UNIT_TESTS=OFF -DENABLE_FUNC_TESTS=OFF $HOME/src/Bear
-    make all
+    make -j16 all
     sudo make install
 end
--- fish/.config/fish/functions/update-neovim.fish
+++ fish/.config/fish/functions/update-neovim.fish
@@ -1,6 +1,6 @@
 function update-neovim
     make distclean
     #make deps
-    make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim" CMAKE_BUILD_TYPE=Release -j16
+    make -j16 CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim" CMAKE_BUILD_TYPE=Release
     sudo make install
 end
