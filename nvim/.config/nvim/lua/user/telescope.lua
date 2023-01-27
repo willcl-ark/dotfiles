e7207c58222532dc41d4cd546d88a05b9b7a255d
willcl-ark <will@256k1.dev>
2023-01-27 08:42:06 +0000

    nvim: improve [S]earch [B]uffer shortcut
    Disable fuzzy finding
    Better keyboard shortcut

--- nvim/.config/nvim/lua/user/telescope.lua
+++ nvim/.config/nvim/lua/user/telescope.lua
@@ -110,14 +110,9 @@ telescope.load_extension('fzf')
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer]' })
-
+vim.keymap.set('n', '<leader>sb', function()
+  require('telescope.builtin').current_buffer_fuzzy_find { fuzzy=false, case_mode="ignore_case" }
+end, { desc = '[S]earch [B]uffer Fuzzily search in current buffer]' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
