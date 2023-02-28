27735924e6f35ff321d1edadef50dd927f578804
willcl-ark <will@256k1.dev>
2023-02-28 08:17:55 +0000

    nvim: telescope find_files use wider preview area

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -114,16 +114,18 @@ return {
         require("telescope.builtin").buffers,
         { desc = "[ ] Find existing buffers" }
       )
-      vim.keymap.set("n", "<leader>sb", function()
-        require("telescope.builtin").current_buffer_fuzzy_find({ fuzzy = false, case_mode = "ignore_case" })
-      end, { desc = "[S]earch [B]uffer Fuzzily search in current buffer]" })
-      vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-      vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+      -- File Pickers
+      vim.keymap.set( "n", "<leader>sf", function() require("telescope.builtin").find_files({ layout_config = { horizontal = { preview_width = 0.75 }, }, }) end, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
       vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-      vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-      vim.keymap.set("n", "<leader>sk", require("telescope.builtin").keymaps, { desc = "[S]earch [K]eymaps" })
+      -- Vim Pickers
+      vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
       vim.keymap.set("n", "<leader>sm", require("telescope.builtin").man_pages, { desc = "[S]earch [M]an pages" })
+      vim.keymap.set("n", "<leader>sq", require("telescope.builtin").quickfix, { desc = "[S]earch [Q]uickfix list" })
+      vim.keymap.set("n", "<leader>sj", require("telescope.builtin").jumplist, { desc = "[S]earch [J]ump list" })
+      vim.keymap.set("n", "<leader>sk", require("telescope.builtin").keymaps, { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>sb", function() require("telescope.builtin").current_buffer_fuzzy_find({ fuzzy = false, case_mode = "ignore_case" }) end, { desc = "[S]earch [B]uffer Fuzzily search in current buffer]" })
+      vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
     end,
   },
