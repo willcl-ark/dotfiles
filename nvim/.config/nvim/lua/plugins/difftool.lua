6a2db06f3dddf0ee7bfb6dd38387e7d3ac3e30f3
willcl-ark <will@256k1.dev>
2023-02-28 10:33:29 +0000

    nvim: add Diffview keymaps and telescope picker action

--- nvim/.config/nvim/lua/plugins/difftool.lua
+++ nvim/.config/nvim/lua/plugins/difftool.lua
@@ -1,12 +1,14 @@
 return {
-  -- Autocompletion
   "sindrets/diffview.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
   event = "VeryLazy",
-  vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "[D]iff [O]pen" } ),
-  vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFileHistory<CR>", { desc = "[D]iff [F]ile history" } ),
-  vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "[D]iff [C]lose" } ),
-  vim.keymap.set("n", "<leader>dp", "<cmd>DiffviewToggleFiles<CR>", { desc = "[D]iff [T]oggle files panel" } ),
+  config = function()
+    require("diffview").setup()
+  end,
+  vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "[D]iff [O]pen" }),
+  vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFileHistory<CR>", { desc = "[D]iff [F]ile history" }),
+  vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "[D]iff [C]lose" }),
+  vim.keymap.set("n", "<leader>dp", "<cmd>DiffviewToggleFiles<CR>", { desc = "[D]iff [T]oggle files panel" }),
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,8 @@ return {
     branch = "0.1.x",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
+      local actions = require("telescope.actions")
+      local actions_state = require("telescope.actions.state")
       require("telescope").setup({
         defaults = {
           layout_strategy = "flex",
@@ -28,72 +30,83 @@ return {
           },
           mappings = {
             i = {
-              ["<C-n>"] = require("telescope.actions").cycle_history_next,
-              ["<C-p>"] = require("telescope.actions").cycle_history_prev,
+              ["<C-n>"] = actions.cycle_history_next,
+              ["<C-p>"] = actions.cycle_history_prev,
 
-              ["<C-j>"] = require("telescope.actions").move_selection_next,
-              ["<C-k>"] = require("telescope.actions").move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
+              ["<C-k>"] = actions.move_selection_previous,
 
-              ["<C-c>"] = require("telescope.actions").close,
+              ["<C-c>"] = actions.close,
 
-              ["<Down>"] = require("telescope.actions").move_selection_next,
-              ["<Up>"] = require("telescope.actions").move_selection_previous,
+              ["<Down>"] = actions.move_selection_next,
+              ["<Up>"] = actions.move_selection_previous,
 
-              ["<CR>"] = require("telescope.actions").select_default,
-              ["<C-x>"] = require("telescope.actions").select_horizontal,
-              ["<C-v>"] = require("telescope.actions").select_vertical,
-              ["<C-t>"] = require("telescope.actions").select_tab,
+              ["<CR>"] = actions.select_default,
+              ["<C-x>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+              ["<C-t>"] = actions.select_tab,
 
-              ["<C-u>"] = require("telescope.actions").preview_scrolling_up,
-              ["<C-d>"] = require("telescope.actions").preview_scrolling_down,
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down,
 
-              ["<PageUp>"] = require("telescope.actions").results_scrolling_up,
-              ["<PageDown>"] = require("telescope.actions").results_scrolling_down,
+              ["<PageUp>"] = actions.results_scrolling_up,
+              ["<PageDown>"] = actions.results_scrolling_down,
 
-              ["<Tab>"] = require("telescope.actions").toggle_selection
-                + require("telescope.actions").move_selection_worse,
-              ["<S-Tab>"] = require("telescope.actions").toggle_selection
-                + require("telescope.actions").move_selection_better,
-              ["<C-q>"] = require("telescope.actions").send_to_qflist + require("telescope.actions").open_qflist,
-              ["<M-q>"] = require("telescope.actions").send_selected_to_qflist
-                + require("telescope.actions").open_qflist,
-              ["<C-l>"] = require("telescope.actions").complete_tag,
-              ["<C-_>"] = require("telescope.actions").which_key, -- keys from pressing <C-/>
+              ["<Tab>"] = actions.toggle_selection
+                + actions.move_selection_worse,
+              ["<S-Tab>"] = actions.toggle_selection
+                + actions.move_selection_better,
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+              ["<M-q>"] = actions.send_selected_to_qflist
+                + actions.open_qflist,
+              ["<C-l>"] = actions.complete_tag,
+              ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+              ["<C-o>"] = function()
+                -- Open in diffview
+                local selected_entry = actions_state.get_selected_entry()
+                local value = selected_entry.value
+                -- close Telescope window properly prior to switching windows
+                vim.api.nvim_win_close(0, true)
+                vim.cmd("stopinsert")
+                vim.schedule(function()
+                  vim.cmd(("DiffviewOpen %s^!"):format(value))
+                end)
+              end,
             },
 
             n = {
-              ["<esc>"] = require("telescope.actions").close,
-              ["<CR>"] = require("telescope.actions").select_default,
-              ["<C-x>"] = require("telescope.actions").select_horizontal,
-              ["<C-v>"] = require("telescope.actions").select_vertical,
-              ["<C-t>"] = require("telescope.actions").select_tab,
+              ["<esc>"] = actions.close,
+              ["<CR>"] = actions.select_default,
+              ["<C-x>"] = actions.select_horizontal,
+              ["<C-v>"] = actions.select_vertical,
+              ["<C-t>"] = actions.select_tab,
 
-              ["<Tab>"] = require("telescope.actions").toggle_selection
-                + require("telescope.actions").move_selection_worse,
-              ["<S-Tab>"] = require("telescope.actions").toggle_selection
-                + require("telescope.actions").move_selection_better,
-              ["<C-q>"] = require("telescope.actions").send_to_qflist + require("telescope.actions").open_qflist,
-              ["<M-q>"] = require("telescope.actions").send_selected_to_qflist
-                + require("telescope.actions").open_qflist,
+              ["<Tab>"] = actions.toggle_selection
+                + actions.move_selection_worse,
+              ["<S-Tab>"] = actions.toggle_selection
+                + actions.move_selection_better,
+              ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+              ["<M-q>"] = actions.send_selected_to_qflist
+                + actions.open_qflist,
 
-              ["j"] = require("telescope.actions").move_selection_next,
-              ["k"] = require("telescope.actions").move_selection_previous,
-              ["H"] = require("telescope.actions").move_to_top,
-              ["M"] = require("telescope.actions").move_to_middle,
-              ["L"] = require("telescope.actions").move_to_bottom,
+              ["j"] = actions.move_selection_next,
+              ["k"] = actions.move_selection_previous,
+              ["H"] = actions.move_to_top,
+              ["M"] = actions.move_to_middle,
+              ["L"] = actions.move_to_bottom,
 
-              ["<Down>"] = require("telescope.actions").move_selection_next,
-              ["<Up>"] = require("telescope.actions").move_selection_previous,
-              ["gg"] = require("telescope.actions").move_to_top,
-              ["G"] = require("telescope.actions").move_to_bottom,
+              ["<Down>"] = actions.move_selection_next,
+              ["<Up>"] = actions.move_selection_previous,
+              ["gg"] = actions.move_to_top,
+              ["G"] = actions.move_to_bottom,
 
-              ["<C-u>"] = require("telescope.actions").preview_scrolling_up,
-              ["<C-d>"] = require("telescope.actions").preview_scrolling_down,
+              ["<C-u>"] = actions.preview_scrolling_up,
+              ["<C-d>"] = actions.preview_scrolling_down,
 
-              ["<PageUp>"] = require("telescope.actions").results_scrolling_up,
-              ["<PageDown>"] = require("telescope.actions").results_scrolling_down,
+              ["<PageUp>"] = actions.results_scrolling_up,
+              ["<PageDown>"] = actions.results_scrolling_down,
 
-              ["?"] = require("telescope.actions").which_key,
+              ["?"] = actions.which_key,
             },
           },
         },
@@ -115,16 +128,26 @@ return {
         { desc = "[ ] Find existing buffers" }
       )
       -- File Pickers
-      vim.keymap.set( "n", "<leader>sf", function() require("telescope.builtin").find_files({ layout_config = { horizontal = { preview_width = 0.75 }, }, }) end, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>sf", function()
+        require("telescope.builtin").find_files({
+          layout_config = { horizontal = { preview_width = 0.75 } },
+        })
+      end, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
       vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
       -- Vim Pickers
       vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
       vim.keymap.set("n", "<leader>sm", require("telescope.builtin").man_pages, { desc = "[S]earch [M]an pages" })
       vim.keymap.set("n", "<leader>sq", require("telescope.builtin").quickfix, { desc = "[S]earch [Q]uickfix list" })
+      vim.keymap.set("n", "<leader>sc", require("telescope.builtin").git_commits, { desc = "[S]earch [C]ommits" })
       vim.keymap.set("n", "<leader>sj", require("telescope.builtin").jumplist, { desc = "[S]earch [J]ump list" })
       vim.keymap.set("n", "<leader>sk", require("telescope.builtin").keymaps, { desc = "[S]earch [K]eymaps" })
-      vim.keymap.set("n", "<leader>sb", function() require("telescope.builtin").current_buffer_fuzzy_find({ fuzzy = false, case_mode = "ignore_case" }) end, { desc = "[S]earch [B]uffer Fuzzily search in current buffer]" })
+      vim.keymap.set("n", "<leader>sb", function()
+        require("telescope.builtin").current_buffer_fuzzy_find({
+          fuzzy = false,
+          case_mode = "ignore_case",
+        })
+      end, { desc = "[S]earch [B]uffer Fuzzily search in current buffer]" })
       vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
       vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
     end,
