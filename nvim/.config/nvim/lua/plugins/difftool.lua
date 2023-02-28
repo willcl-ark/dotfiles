9937731acba29ba1ac6d0f89d0792c97fc36b26a
willcl-ark <will@256k1.dev>
2023-02-28 10:09:57 +0000

    nvim: add difftool.nvim

---
+++ nvim/.config/nvim/lua/plugins/difftool.lua
@@ -0,0 +1,12 @@
+return {
+  -- Autocompletion
+  "sindrets/diffview.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  event = "VeryLazy",
+  vim.keymap.set("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "[D]iff [O]pen" } ),
+  vim.keymap.set("n", "<leader>df", "<cmd>DiffviewFileHistory<CR>", { desc = "[D]iff [F]ile history" } ),
+  vim.keymap.set("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "[D]iff [C]lose" } ),
+  vim.keymap.set("n", "<leader>dp", "<cmd>DiffviewToggleFiles<CR>", { desc = "[D]iff [T]oggle files panel" } ),
+}
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -39,7 +39,7 @@ return {
       nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
       nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementations")
       nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinitions")
-      nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+      nmap("<leader>gs", require("telescope.builtin").lsp_document_symbols, "[G]oto [S]ymbols")
       nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
       -- See `:help K` for why this keymap
