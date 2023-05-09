19db1a8a14a24f5214dfe69f0c53e59913d499aa
willcl-ark <will@256k1.dev>
2023-05-09 13:10:15 +0100

    fish: add 6-KEF to SSID

--- fish/.config/fish/functions/sshnuc.fish
+++ fish/.config/fish/functions/sshnuc.fish
@@ -1,7 +1,7 @@
 # Defined in /var/folders/0d/tgscg8cj0rx035gf2lvl15fh0000gn/T//fish.YKkbXL/sshnuc.fish @ line 2
 function sshnuc
     switch (echo "$SSID")
-        case "6" "6-5GHz"
+        case "6" "6-5GHz" "6-KEF"
             ssh nuclocal
         case '*'
             ssh nucremote
