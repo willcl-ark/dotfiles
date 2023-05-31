66eca464dec039fa2b632b99788c58f931017244
willcl-ark <will@256k1.dev>
2023-05-31 15:27:28 +0100

    fish: add fmake-lint-check-test

---
+++ fish/.config/fish/functions/fmake-lint-check-test.fish
@@ -0,0 +1,25 @@
+function fmake-lint-check-test
+
+    bear -- make -j16; or begin
+        notify-send "bear -- make -j16 failed"
+        return
+    end
+
+    ./test/lint/all-lint.py; or begin
+        notify-send "./test/lint/all-lint.py failed"
+        return
+    end
+
+    make -j16 check; or begin
+        notify-send "make -j16 check failed"
+        return
+    end
+
+    test/functional/test_runner.py --jobs=32 --cachedir=/mnt/tmp/cache --tmpdir=/mnt/tmp; or begin
+        notify-send "test/functional/test_runner.py failed"
+        return
+    end
+
+    notify-send "make-lint-check-test complete"
+
+end
