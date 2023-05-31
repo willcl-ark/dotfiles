4565b6578c50aced41efa39e7835f46207962ea7
willcl-ark <will@256k1.dev>
2023-05-31 15:18:53 +0100

    fish: ytdlp permit ctrl+c exit

--- fish/.config/fish/functions/ytdlp.fish
+++ fish/.config/fish/functions/ytdlp.fish
@@ -1,11 +1,21 @@
 function ytdlp
-    read --prompt-str "Enter video URL: " url
+    # Trap to catch Ctrl+C
+    function handle_interrupt
+        echo "Operation interrupted by user."
+        exit 1
+    end
+    trap handle_interrupt INT
+
+    read --prompt-str "Enter video URL: " url || return
     yt-dlp -F $url
-    read --prompt-str "Enter format: " format
-    read --prompt-str "Embed Thumbnail? (y/n)" thumb
-    if test "$thumb" = "y" -o "Y"
+    read --prompt-str "Enter format: " format || return
+    read --prompt-str "Embed Thumbnail? (y/n)" thumb || return
+    if test "$thumb" = "y" -o "$thumb" = "Y"
         yt-dlp -f $format $url --embed-thumbnail
     else
         yt-dlp -f $format $url
     end
+
+    # Remove the trap
+    trap - INT
 end
