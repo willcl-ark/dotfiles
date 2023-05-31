03215ebc0aef3704da28d96da1471214728ba8c1
willcl-ark <will@256k1.dev>
2023-05-31 15:18:12 +0100

    fish: core-guix-build
    Fix signer name
    Split into sub-commands using argparse

--- fish/.config/fish/functions/core-guix-build.fish
+++ fish/.config/fish/functions/core-guix-build.fish
@@ -1,67 +1,74 @@
 function core-guix-build
-    argparse 'f/force' -- $argv
+    argparse 'c/codesigned' 'n/non-codesigned' 'a/all' -- $argv
 
-    if not set -q _flag_force
-        if not string match --regex --quiet 'bitcoin' $PWD
-            echo "CWD appears incorrect: $PWD"
-            return 1
-        end
+    if set -q _flag_all
+        set _flag_non-codesigned
+        set _flag_codesigned
     end
 
-    set --local vars SIGNER GUIX_SIGS_REPO DETACHED_SIGS_REPO VERSION
-    for v in $vars
+    set --local required_vars SIGNER GUIX_SIGS_REPO DETACHED_SIGS_REPO VERSION
+    set --local verr
+    for v in $required_vars
         if not set -q $v
             echo "$v not set"
-            set --local verr
+            set verr 1
         end
     end
-    if set -q verr
+    if test -n "$verr"
         return 1
     end
 
+    # Set name by splitting SIGNER
+    set --local SIGNER_NAME (string split "=" $SIGNER)[2]
+
     # Prepare repo
-    git fetch --all
+    git fetch upstream --tags
     git checkout v$VERSION
 
-    # Build
-    ./contrib/guix/guix-build
+    if set -q _flag_non-codesigned
 
-    # Attest non-codesigned
-    ./contrib/guix/guix-attest
-    pushd $GUIX_SIGS_REPO
-    git switch main
-    git pull
-    git checkout -b {$VERSION}-non-codesigned
-    git add $VERSION
-    git commit -m "Add attestations by $SIGNER for $VERSION non-codesigned"
-    git push
-    popd
-    xdg-open https://github.com/bitcoin-core/guix.sigs
+        # Build
+        ./contrib/guix/guix-build
 
-    # Sign binaries
-    pushd $DETACHED_SIGS_REPO
-    git fetch
-    git checkout v$VERSION
-    popd
-    ./contrib/guix/guix-codesign
+        # Attest non-codesigned
+        ./contrib/guix/guix-attest
+        pushd $GUIX_SIGS_REPO
+        git switch main
+        git pull
+        git checkout -b {$VERSION}-non-codesigned
+        git add $VERSION
+        git commit -m "Add attestations by $SIGNER_NAME for $VERSION non-codesigned"
+        git push --set-upstream origin $VERSION-non-codesigned
+        popd
+    end
 
-    # Attest codesigned binaries
-    ./contrib/guix/guix-attest
-    pushd $GUIX_SIGS_REPO
-    git switch main
-    git pull
-    git checkout -b {$VERSION}-codesigned
-    git add $VERSION
-    git commit -m "Add attestations by $SIGNER for $VERSION codesigned"
-    git push
-    popd
-    xdg-open https://github.com/bitcoin-core/guix.sigs
+    if set -q _flag_codesigned
+
+        # Sign binaries
+        pushd $DETACHED_SIGS_REPO
+        git fetch
+        git checkout v$VERSION
+        popd
+        ./contrib/guix/guix-codesign
+
+        # Attest codesigned binaries
+        ./contrib/guix/guix-attest
+        pushd $GUIX_SIGS_REPO
+        git switch main
+        git pull
+        git checkout -b {$VERSION}-codesigned
+        git add $VERSION
+        git commit -m "Add attestations by $SIGNER_NAME for $VERSION codesigned"
+        git push --set-upstream origin $VERSION-codesigned
+        popd
+        xdg-open https://github.com/bitcoin-core/guix.sigs
+    end
 
     # Verify sigs
     pushd $GUIX_SIGS_REPO
     git switch main
     git pull
     popd
-    ./contrib/guix/guix-verify
+    SIGNER=$SIGNER_NAME ./contrib/guix/guix-verify
 
 end
