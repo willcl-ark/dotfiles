8db456a9f5e797bc77d935e380a9bafa15426d52
willcl-ark <will@256k1.dev>
2023-12-07 20:31:02 +0000

    update fmake-lint-check-test

--- fish/.config/fish/functions/fmake-lint-check-test.fish
+++ fish/.config/fish/functions/fmake-lint-check-test.fish
@@ -1,13 +1,22 @@
 function fmake-lint-check-test
 
-    bear -- make -j16; or begin
-        notify-send "bear -- make -j16 failed"
-        return
-    end
-
     ./test/lint/all-lint.py; or begin
         notify-send "./test/lint/all-lint.py failed"
         return
+    end
+
+    if test -d test/lint/test_runner/
+        pushd test/lint/test_runner/
+        cargo fmt
+        cargo clippy
+        cargo run; or begin
+            notify-send "Rust linter failed"
+            return
+        end
+        popd
+    else
+        # Optional: Notify if the directory does not exist
+        notify-send "Not running Rust linter. test/lint/test_runner/ directory does not exist"
     end
 
     make -j16 check; or begin
