8f446892a8ab7668de36354c89852969e39275eb
willcl-ark <will@256k1.dev>
2026-02-10 22:29:27 +0000

    fix ytdlp function

--- fish/.config/fish/functions/ytdlp.fish
+++ fish/.config/fish/functions/ytdlp.fish
@@ -1,24 +1,35 @@
 function ytdlp
+
     # Trap to catch Ctrl+C
     function handle_interrupt
-        echo "Operation interrupted by user."
         exit 1
     end
     trap handle_interrupt INT
 
     read --prompt-str "Enter video URL: " url || return
-    yt-dlp -F $url
-    read --prompt-str "Enter format: " format || return
-    read --prompt-str "Embed Thumbnail? (y/n)" thumb || return
+    if not string match -q "http*" $url
+        echo "Invalid URL format"
+        return 1
+    end
+
+    yt-dlp -F $url || return
+
+    read --prompt-str "Enter format (e.g. 137+251 for 1080p video + best audio): " format || return
+    if test -z "$format"
+        echo "Format cannot be empty"
+        return 1
+    end
+
+    read --prompt-str "Embed Thumbnail? (y/n) " thumb || return
     if test "$thumb" = "y" -o "$thumb" = "Y"
         yt-dlp -f $format $url \
             --embed-thumbnail \
-            --restrict-filenames
-            # --cookies-from-browser firefox:/home/will/snap/firefox/common/.mozilla/firefox/b2pboypq.default-release
+            --restrict-filenames \
+            --cookies-from-browser firefox
     else
         yt-dlp -f $format $url \
-            --restrict-filenames
-            # --cookies-from-browser firefox:/home/will/snap/firefox/common/.mozilla/firefox/b2pboypq.default-release
+            --restrict-filenames \
+            --cookies-from-browser firefox
     end
 
     # Remove the trap
